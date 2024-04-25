8784b029445022cd905abdb109152f9995937317
OpchetZ <varanyookasamsuk@gmail.com>
2024-04-25 17:12:53 +0700

    continue pdf but vaca

--- resources/views/components/boost-pdf.blade.php
+++ resources/views/components/boost-pdf.blade.php
@@ -4,16 +4,62 @@
     <meta http-equiv="Content-Language" content="th" />
     <meta charset="UTF-8">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    
     <!-- Bootstrap 3.x only : DOMPDF support float, not flexbox -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css"
         integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
     <!-- thai font -->
     <link href="https://fonts.googleapis.com/css2?family=Sarabun" rel="stylesheet">
+    
     <style>
+        @font-face {
+            font-family: 'THSarabunNew';
+            font-style: normal;
+            font-weight: normal;
+            src:url("{{ asset('fonts/THSarabuNew.ttf') }}") format('truetype');
+        }
+        @font-face {
+            font-family: 'THSarabunNew';
+            font-style: normal;
+            font-weight: bold;
+            src:url("{{ asset('fonts/THSarabuNew Bold.ttf') }}") format('truetype');
+        }
+        @font-face {
+            font-family: 'THSarabunNew';
+            font-style: italic;
+            font-weight: normal;
+            src:url("{{ asset('fonts/THSarabuNew italic.ttf') }}") format('truetype');
+        }
+        @font-face {
+            font-family: 'THSarabunNew';
+            font-style: italic;
+            font-weight: bold;
+            src:url("{{ asset('fonts/THSarabuNew Bolditalic.ttf') }}") format('truetype');
+        }
         body {
             font-family: 'Sarabun', sans-serif;
             font-size: 16px;
         }
+        /* table {
+            font-family: 'THSarabunNew';
+            border-collapse: collapse;
+            
+        }
+        td,th{
+            border: 1px solid;
+        } */
+        .b {
+            border: 1px solid;
+            width:60px;
+        }
+        .a {
+            border: 1px solid;
+            
+        }
+        /* .col-xs-6 {
+            border: 1px solid;
+            
+        } */
     </style>
 </head>
 
--- resources/views/leaverequest/pdf.blade.php
+++ resources/views/leaverequest/pdf.blade.php
@@ -1,5 +1,8 @@
 <x-boost-pdf title="">
     <div class="container">
+        @if ($leaverequest->leave_type_name =='ลาพักผ่อน')
+            
+        
         <div class="row">
             <div class="col-xs-5">
                 <div>{{ $employs->status->status_name }}</div>
@@ -23,42 +26,190 @@
             </div>
         </div>
         @php
-            $vacatotal = ($employs->Acc_vaca_day + $employs->vaca_max)   
+            $vacatotal = $employs->Acc_vaca_day + $employs->vaca_max;
         @endphp
         <div class="container">
             <div class="text-right">
-            <span> ข้าพเจ้า........{{ $leaverequest->employ->name }}...........ตำแหน่ง.......{{ $employs->position->Job_position }}...........................</span>
+                <span>
+                    ข้าพเจ้า........{{ $leaverequest->employ->name }}...........ตำแหน่ง.......{{ $employs->position->Job_position }}...........................</span>
             </div>
             <span>สังกัด...........โรงพยาบาลอ่างทอง...........................กลุ่มงาน......................................................................</span>
-            <span>มีวันลาพักผ่อนสะสม..{{ $employs->Acc_vaca_day }}..วันทำการ มีสิทธิลาพักผ่อนประจำปีนี้อีก..{{ $employs->vaca_max }}..วันทำการ รวมเป็น{{ $vacatotal }}วันทำการ</span>
-            <span>ขอลาพักผ่อนตั้งแต่{{ $leaverequest->start_date->thaidate('วันที่ j เดือน M พ.ศ. y') }} ถึงวันที่ {{ $leaverequest->start_date->thaidate('j เดือน M พ.ศ. y') }} 
+            <span>มีวันลาพักผ่อนสะสม..{{ $employs->Acc_vaca_day }}..วันทำการ
+                มีสิทธิลาพักผ่อนประจำปีนี้อีก..{{ $employs->vaca_max }}..วันทำการ
+                รวมเป็น{{ $vacatotal }}วันทำการ</span>
+            <span>ขอลาพักผ่อนตั้งแต่{{ $leaverequest->start_date->thaidate('วันที่ j เดือน M พ.ศ. y') }} ถึงวันที่
+                {{ $leaverequest->start_date->thaidate('j เดือน M พ.ศ. y') }}
                 มีกำหนด {{ $leaverequest->total_leave }} วัน</span>
-            <span>ในระหว่างการลาติดต่อข้าพเจ้าได้ที่.......................................................................................................</span> <br>
+            <span>ในระหว่างการลาติดต่อข้าพเจ้าได้ที่.......................................................................................................</span>
+            <br>
             <span>..........................................................หมายเลขโทรศัพท์.............{{ $employs->phone }}....................................</span>
         </div>
         <br>
         <div class="row">
             <div class="col-xs-6"></div>
             <div class="col-xs-6">
-                <span>    ขอแสดงความนับถือ</span><br>
+                <span> ขอแสดงความนับถือ</span><br>
                 <span>ลงชื่อ.................................</span><br>
-                <span>(...........................................)</span>
+                <span>(.......{{ $leaverequest->employ->name }}........)</span>
+            </div>
+        </div>
+        <div class="row">
+            <div>
+                <div class="text-left">
+                    <span>สถิติการลาในงบปีนี้</span>
+                </div>
+                <div class="row">
+                    <div class="col-xs-4 b">ลามาแล้ว<br>(วันทำการ)</div>
+                    <div class="col-xs-4 b">ลาครั้งนี้<br>(วันทำการ)</div>
+                    <div class="col-xs-4 b">รวมเป็น<br>(วันทำการ)</div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-4 b">20<br></div>
+                    <div class="col-xs-4 b">20 <br> </div>
+                    <div class="col-xs-4 b">20</div>
+                </div>
+
+            </div>
+
+
+        </div>
+        <div class="row">
+            <div class="col-xs-6"></div>
+            <div class="col-xs-6">
+                <span> ความเห็นผู้บังคับบัญชา</span><br>
+                <span>.........................................................</span> <br>
+                <span>ลงชื่อ................................................</span><br>
+                <span>ตำแหน่ง...........................................</span> <br>
+                <span>วันที่.................................................</span>
             </div>
         </div>
         <div class="row">
             <div class="col-xs-6">
-                <div class="text-center">
-                <span>สถิติการลาในงบปีนี้</span>
-                </div>
-                <table class="table">
-                        <tr>
-                            <th scope="col"><p style="font-family: Arial, sans-serif">ลามาแล้ว<br>(วันทำการ)</p></th>
-                            <th>ลาครั้งนี้<br>(วันทำการ)</th>
-                            <th>รวมเป็น<br>(วันทำการ)</th>
-                        </tr>
-                </table>
-                <span>กินไก่</span>
+                
+                <span>ลงชื่อ.....................................ผู้ตรวจสอบ</span> <br>
+                <span>ตำแหน่ง...........................................</span> <br>
+                <span>วันที่.................................................</span>
+            </div>
+            <div class="col-xs-6">
+                <span> คำสั่ง ( ) อนุญาต ( ) ไม่อนุญาต</span><br>
+                <span>.........................................................</span> <br>
+                <span>ลงชื่อ................................................</span><br>
+                <span>ตำแหน่ง...........................................</span> <br>
+                <span>วันที่.................................................</span>
             </div>
         </div>
+        <span>หมายเหตุ : ห้ามลบขีดเขียน</span>
+        @else
+        <div class="row">
+            <div class="col-xs-5">
+                <div>{{ $employs->status->status_name }}</div>
+            </div>
+            <br>
+            <div class="col-xs-6 text-right">
+                <div>เขียนที่ โรงพยาบาลอ่างทอง</div>
+                <div>{{ $leaverequest->created_at->thaidate('วันที่ j เดือน F พ.ศ. y') }}</div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-6">
+                <div>
+                    <span> เรื่อง </span>
+                    <span> ขอ{{ $leaverequest->leave_type_name }} </span>
+                </div>
+                <div>
+                    <span> เรียน </span>
+                    <span> ผู้อำนวยการโรงพยาบาลอ่างทอง </span>
+                </div>
+            </div>
+        </div>
+        @php
+            $vacatotal = $employs->Acc_vaca_day + $employs->vaca_max;
+        @endphp
+        <div class="container">
+            <div class="text-right">
+                <span>
+                    ข้าพเจ้า........{{ $leaverequest->employ->name }}...........ตำแหน่ง.......{{ $employs->position->Job_position }}...........................</span>
+            </div>
+            <span>สังกัด...........โรงพยาบาลอ่างทอง...........................กลุ่มงาน......................................................................</span>
+            <span>มีวันลาพักผ่อนสะสม..{{ $employs->Acc_vaca_day }}..วันทำการ
+                มีสิทธิลาพักผ่อนประจำปีนี้อีก..{{ $employs->vaca_max }}..วันทำการ
+                รวมเป็น{{ $vacatotal }}วันทำการ</span>
+            <span>ขอลาพักผ่อนตั้งแต่{{ $leaverequest->start_date->thaidate('วันที่ j เดือน M พ.ศ. y') }} ถึงวันที่
+                {{ $leaverequest->start_date->thaidate('j เดือน M พ.ศ. y') }}
+                มีกำหนด {{ $leaverequest->total_leave }} วัน</span>
+            <span>ในระหว่างการลาติดต่อข้าพเจ้าได้ที่.......................................................................................................</span>
+            <br>
+            <span>..........................................................หมายเลขโทรศัพท์.............{{ $employs->phone }}....................................</span>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-xs-6"></div>
+            <div class="col-xs-6">
+                <span> ขอแสดงความนับถือ</span><br>
+                <span>ลงชื่อ.................................</span><br>
+                <span>(.......{{ $leaverequest->employ->name }}........)</span>
+            </div>
+        </div>
+        <div class="row">
+            <div>
+                <div class="text-left">
+                    <span>สถิติการลาในปีงบประมาณนี้</span>
+                </div>
+                <div class="row">
+                    <div class="col-xs-4 b">ประเภท<br>การลา </div>
+                    <div class="col-xs-4 b">ลามาแล้ว<br>(วันทำการ)</div>
+                    <div class="col-xs-4 b">ลาครั้งนี้<br>(วันทำการ)</div>
+                    <div class="col-xs-4 b">รวมเป็น<br>(วันทำการ)</div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-4 b">ป่วย</div>
+                    <div class="col-xs-4 b">20</div>
+                    <div class="col-xs-4 b">20 </div>
+                    <div class="col-xs-4 b">20</div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-4 b">กิจส่วนตัว</div>
+                    <div class="col-xs-4 b">20</div>
+                    <div class="col-xs-4 b">20 </div>
+                    <div class="col-xs-4 b">20</div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-4 b">คลอดบุตร</div>
+                    <div class="col-xs-4 b">20</div>
+                    <div class="col-xs-4 b">20 </div>
+                    <div class="col-xs-4 b">20</div>
+                </div>
 
-        </x-bootstrap-pdf>
+            </div>
+
+
+        </div>
+        <div class="row">
+            <div class="col-xs-6"></div>
+            <div class="col-xs-6">
+                <span> ความเห็นผู้บังคับบัญชา</span><br>
+                <span>.........................................................</span> <br>
+                <span>ลงชื่อ................................................</span><br>
+                <span>ตำแหน่ง...........................................</span> <br>
+                <span>วันที่.................................................</span>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-6">
+                
+                <span>ลงชื่อ.....................................ผู้ตรวจสอบ</span> <br>
+                <span>ตำแหน่ง...........................................</span> <br>
+                <span>วันที่.................................................</span>
+            </div>
+            <div class="col-xs-6">
+                <span> คำสั่ง ( ) อนุญาต ( ) ไม่อนุญาต</span><br>
+                <span>.........................................................</span> <br>
+                <span>ลงชื่อ................................................</span><br>
+                <span>ตำแหน่ง...........................................</span> <br>
+                <span>วันที่.................................................</span>
+            </div>
+        </div>
+        <span>หมายเหตุ : ห้ามลบขีดเขียน</span>
+        @endif
+    </div>
+    </x-bootstrap-pdf>
